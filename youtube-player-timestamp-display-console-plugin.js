d1a84ca4ef881099e5f33c20ab0ba60d2a90f358
Sam <144938568+SamLo2200@users.noreply.github.com>
2024-03-18 16:45:48 +0800

    added timestamp display function using p tag

--- youtube-player-timestamp-display-console-plugin.js
+++ youtube-player-timestamp-display-console-plugin.js
@@ -1,9 +1,24 @@
+// Init a p tag for displaying currentTimestamp under the video title.
+
+const mainDiv = document.querySelector("ytd-watch-metadata").querySelector("#title");
+const timestampTag = document.createElement("p");
+
+timestampTag.setAttribute(
+    "style",
+    "font-family: 'Roboto', 'Arial', sans-serif; font-size: 1.53rem; padding-top: 2px; padding-bottom: 12px;"
+);
+
+timestampTag.textContent = "initialized";
+mainDiv.appendChild(timestampTag);
+
+//Declaration
 ytplayer = document.getElementById("movie_player");
 
 let previousTimestamp = "";
 let currentTimestamp = "";
 
 function getCurrentTimestamp(playHeadInSecond) {
+    //Using Date Object to calculate the timestamp with the playhead second given by the ytplayer.
     timestampObj = new Date(playHeadInSecond * 1000);
     hours = timestampObj.getUTCHours();
     minutes = timestampObj.getUTCMinutes();
@@ -19,19 +34,14 @@ function getCurrentTimestamp(playHeadInSecond) {
     return timestamp;
 }
 
+//Update the timestamp every 10ms.
 const printTimestamp = setInterval(function () {
     let playHeadSecond = ytplayer.getCurrentTime();
     currentTimestamp = getCurrentTimestamp(playHeadSecond);
     if (previousTimestamp !== currentTimestamp) {
-        console.log(currentTimestamp);
+        // console.log(currentTimestamp);
+        //
+        timestampTag.textContent = currentTimestamp;
         previousTimestamp = currentTimestamp;
     }
 }, 10);
-
-//todo: append timestamp to youtube.com
-
-// const ytdWatchMetadata = document.getElementById("title");
-// const timestampTag = document.createElement("p");
-// timestampTag.textContent = "initialized";
-// ytdWatchMetadata.appendChild(timestampTag);
-// timestampTag.textContent = "changed";
