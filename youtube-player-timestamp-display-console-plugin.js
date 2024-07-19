26f33e44cd9413cbc520d96c3d23d64910b845aa
Sam <144938568+SamLo2200@users.noreply.github.com>
2024-07-20 04:49:27 +0800

    added multiple pairs of timestamps looping functionality

--- youtube-player-timestamp-display-console-plugin.js
+++ youtube-player-timestamp-display-console-plugin.js
@@ -12,88 +12,106 @@ let playingType = "";
 let isLooping = false;
 
 function toSecond(hours, minutes, seconds) {
-    return (parseInt(hours) * 60 * 60 + parseInt(minutes) * 60 + parseInt(seconds));
+  return parseInt(hours) * 60 * 60 + parseInt(minutes) * 60 + parseInt(seconds);
 }
 
 class TimestringSplitter {
-    static single(timeString) {
-        //Requires deconstruction: [hours, minutes, seconds]
-        return timeString.split(":");
-    }
+  static single(timeString) {
+    //Requires deconstruction: [hours, minutes, seconds]
+    return timeString.split(":");
+  }
 
-    static range(timeString) {
-        const [pointA, pointB] = timeString.split("-");
+  static range(timeString) {
+    const [pointA, pointB] = timeString.split("-");
 
-        const [pointAHours, pointAMinutes, pointASeconds] = TimestringSplitter.single(pointA);
-        const [pointBHours, pointBMinutes, pointBSeconds] = TimestringSplitter.single(pointB);
+    const [pointAHours, pointAMinutes, pointASeconds] =
+      TimestringSplitter.single(pointA);
+    const [pointBHours, pointBMinutes, pointBSeconds] =
+      TimestringSplitter.single(pointB);
 
-        return [
-            {hours: pointAHours, minutes: pointAMinutes, seconds: pointASeconds,},
-            {hours: pointBHours, minutes: pointBMinutes, seconds: pointBSeconds,},
-        ];
-    }
+    return [
+      { hours: pointAHours, minutes: pointAMinutes, seconds: pointASeconds },
+      { hours: pointBHours, minutes: pointBMinutes, seconds: pointBSeconds },
+    ];
+  }
 }
 
 function looper(timeStringInRange) {
-    let playlistArray = [];
+  let playlistArray = [];
+  let playlistIndex = 0;
 
-    // Parse multiple inputs and add each timestamp pair to playlistArray as an object
-    timeStringInRange.split(",").forEach(playlistItemTS => {
-        const [pointATime, pointBTime] = TimestringSplitter.range(playlistItemTS);
+  // Parse multiple inputs that seperated by comma and add each timestamp pair to playlistArray as an object
+  timeStringInRange.split(",").forEach((playlistItemTS) => {
+    const [pointATime, pointBTime] = TimestringSplitter.range(playlistItemTS);
 
-        // Convert returned timestamp to seconds
-        let pointAInSeconds = toSecond(pointATime.hours, pointATime.minutes, pointATime.seconds);
-        let pointBInSeconds = toSecond(pointBTime.hours, pointBTime.minutes, pointBTime.seconds);
+    // Convert returned timestamp to seconds
+    let pointAInSeconds = toSecond(
+      pointATime.hours,
+      pointATime.minutes,
+      pointATime.seconds,
+    );
+    let pointBInSeconds = toSecond(
+      pointBTime.hours,
+      pointBTime.minutes,
+      pointBTime.seconds,
+    );
 
-        // Push the total seconds to the array as an record
-        playlistArray.push({pointAInSeconds: pointAInSeconds, pointBInSeconds: pointBInSeconds});
+    // Push the total seconds to the array as an record
+    playlistArray.push({
+      pointAInSeconds: pointAInSeconds,
+      pointBInSeconds: pointBInSeconds,
     });
+  });
 
-    console.log(playlistArray);
+  console.log(playlistArray);
 
-    const looping = setInterval(() => {
+  // Interval that handles looping
+  const looping = setInterval(() => {
+    // If the playhead hits the pointBInSeconds + the record is not the last in the arraylist: skip to next record
+    if (
+      playHeadSecond >= playlistArray[playlistIndex].pointBInSeconds &&
+      playlistIndex < playlistArray.length - 1
+    ) {
+      playlistIndex += 1;
+      ytplayer.seekTo(playlistArray[playlistIndex].pointAInSeconds);
+    }
 
-        // Two cases:
-        // 1. If the player head hit the end of pointBInSecond !== end of the playlist => next record timestamp A
-        // 2. If the player head hit the end of pointBInSecond + end of the playlist => first record timestamp A
+    // If the playhead hits pointBInSeconds + the record is the last in the arraylist: Go to the beginning of the array
+    if (
+      playHeadSecond >= playlistArray[playlistIndex].pointBInSeconds &&
+      playlistIndex === playlistArray.length - 1
+    ) {
+      playlistIndex = 0;
+      ytplayer.seekTo(playlistArray[playlistIndex].pointAInSeconds);
+    }
 
-        for (let i = 0; i < playlistArray.length; i++) {
-            const pointAInSeconds = playlistArray[i].pointAInSeconds;
-            const pointBInSeconds = playlistArray[i].pointBInSeconds;
+    // Skip to the first timestamp when clicking the start button
+    if (playHeadSecond <= playlistArray[0].pointAInSeconds) {
+      playlistIndex = 0;
+      ytplayer.seekTo(playlistArray[playlistIndex].pointAInSeconds);
+    }
 
-            if (playHeadSecond <= pointAInSeconds) {
-                ytplayer.seekTo(pointAInSeconds);
-            }
-        }
-
-        if (playHeadSecond <= pointAInSeconds) {
-            ytplayer.seekTo(pointAInSeconds);
-        }
-
-        if (playHeadSecond >= pointBInSeconds) {
-            ytplayer.seekTo(pointAInSeconds);
-        }
-
-        // Execute clearInterval when the stop button is clicked
-        if (isLooping === false) {
-            clearInterval(looping);
-        }
-    }, 50);
-
-    //To do: Add a stop button to stop this looping
+    // Execute clearInterval when the stop button is clicked
+    if (isLooping === false) {
+      clearInterval(looping);
+    }
+  }, 50);
 }
 
 // Init a p tag for displaying currentTimestamp under the video title.
 
 const mainDiv = document
-    .querySelector("ytd-watch-metadata")
-    .querySelector("#title");
+  .querySelector("ytd-watch-metadata")
+  .querySelector("#title");
 const pluginDiv = document.createElement("div");
 const timestampTag = document.createElement("p");
 pluginDiv.classList.add("plugin-div");
 timestampTag.classList.add("timestamp");
 
-timestampTag.setAttribute("style", "font-family: 'Roboto', 'Arial', sans-serif; font-size: 1.53rem; padding-top: 6px; padding-bottom: 5px;");
+timestampTag.setAttribute(
+  "style",
+  "font-family: 'Roboto', 'Arial', sans-serif; font-size: 1.53rem; padding-top: 6px; padding-bottom: 5px;",
+);
 
 //Create a form
 const inputForm = document.createElement("form");
@@ -112,44 +130,47 @@ inputForm.appendChild(submitButton);
 
 // Event listener for form submission
 inputForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
-    document.getElementById("timestamp-input-field").blur(); //Remove focus from the input field
+  event.preventDefault(); // Prevent the default form submission
+  document.getElementById("timestamp-input-field").blur(); //Remove focus from the input field
 
-    // Get form data
-    const formData = new FormData(event.currentTarget);
-    let userInput = formData.get("timestamp");
+  // Get form data
+  const formData = new FormData(event.currentTarget);
+  let userInput = formData.get("timestamp");
 
-    if (isLooping === true) {
-        isLooping = false;
-        submitButton.textContent = "Confirm";
-        console.log(isLooping);
-    } else {
-        if (userInput.length <= 8) {
-            playingType = "directSkip";
-            const [hours, minutes, seconds] = TimestringSplitter.single(userInput);
-            ytplayer.seekTo(toSecond(hours, minutes, seconds));
-        }
-
-        if (userInput.length > 8 && isLooping !== true) {
-            playingType = "looping";
-            isLooping = true;
-            looper(userInput);
-
-            submitButton.textContent = "Stop/Reset";
-        }
+  if (isLooping === true) {
+    isLooping = false;
+    submitButton.textContent = "Confirm";
+    console.log(isLooping);
+  } else {
+    if (userInput.length <= 8) {
+      playingType = "directSkip";
+      const [hours, minutes, seconds] = TimestringSplitter.single(userInput);
+      ytplayer.seekTo(toSecond(hours, minutes, seconds));
     }
 
-    // Display form data
+    if (userInput.length > 8 && isLooping !== true) {
+      playingType = "looping";
+      isLooping = true;
+      looper(userInput);
 
-    const formDataObject = {};
-    formData.forEach(function (value, key) {
-        formDataObject[key] = value;
-    });
+      submitButton.textContent = "Stop/Reset";
+    }
+  }
 
-    console.log(formDataObject);
+  // Display form data
+
+  const formDataObject = {};
+  formData.forEach(function (value, key) {
+    formDataObject[key] = value;
+  });
+
+  console.log(formDataObject);
 });
 
-inputForm.setAttribute("style", "font-family: 'Roboto', 'Arial', sans-serif; font-size: 1.53rem; padding-top: 0.2px; padding-bottom: 13px;");
+inputForm.setAttribute(
+  "style",
+  "font-family: 'Roboto', 'Arial', sans-serif; font-size: 1.53rem; padding-top: 0.2px; padding-bottom: 13px;",
+);
 
 //Append all elements created above
 mainDiv.appendChild(pluginDiv);
@@ -158,57 +179,71 @@ pluginDiv.appendChild(inputForm);
 timestampTag.textContent = "initialized";
 
 function getCurrentTimestamp(playHeadInSecond) {
-    //Using Date Object to calculate the timestamp with the playhead second given by the ytplayer.
-    timestampObj = new Date(playHeadInSecond * 1000);
-    hours = timestampObj.getUTCHours();
-    minutes = timestampObj.getUTCMinutes();
-    seconds = timestampObj.getSeconds();
+  //Using Date Object to calculate the timestamp with the playhead second given by the ytplayer.
+  timestampObj = new Date(playHeadInSecond * 1000);
+  hours = timestampObj.getUTCHours();
+  minutes = timestampObj.getUTCMinutes();
+  seconds = timestampObj.getSeconds();
 
-    timestamp = hours.toString() + ":" + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+  timestamp =
+    hours.toString() +
+    ":" +
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
 
-    return timestamp;
+  return timestamp;
 }
 
 //Update the timestamp
 const printTimestamp = () => {
-    playHeadSecond = ytplayer.getCurrentTime();
-    currentTimestamp = getCurrentTimestamp(playHeadSecond);
-    if (previousTimestamp !== currentTimestamp) {
-        // console.log(currentTimestamp)
-        timestampTag.textContent = currentTimestamp;
-        previousTimestamp = currentTimestamp;
-    }
+  playHeadSecond = ytplayer.getCurrentTime();
+  currentTimestamp = getCurrentTimestamp(playHeadSecond);
+  if (previousTimestamp !== currentTimestamp) {
+    // console.log(currentTimestamp)
+    timestampTag.textContent = currentTimestamp;
+    previousTimestamp = currentTimestamp;
+  }
 };
 
 const mainLoop = setInterval(function () {
-    printTimestamp();
+  printTimestamp();
 }, 10);
 
 //Shortcuts
 document.addEventListener("keydown", function (event) {
-    // Define focused HTML elements to ignore
-    const formElements = ["INPUT", "TEXTAREA", "SELECT", "OPTION", "DIV"];
+  // Define focused HTML elements to ignore
+  const formElements = ["INPUT", "TEXTAREA", "SELECT", "OPTION", "DIV"];
 
-    //Copy currentTimestamp to clipboard when Q is pressed
-    if ((event.key === "q" || event.key === "Q" || event.key === "手") && !formElements.includes(event.target.tagName)) {
-        navigator.clipboard.writeText(currentTimestamp);
-    }
+  //Copy currentTimestamp to clipboard when Q is pressed
+  if (
+    (event.key === "q" || event.key === "Q" || event.key === "手") &&
+    !formElements.includes(event.target.tagName)
+  ) {
+    navigator.clipboard.writeText(currentTimestamp);
+  }
 
-    //Select timestamp input field
-    if ((event.key === "w" || event.key === "W" || event.key === "田") && !formElements.includes(event.target.tagName)) {
-        event.preventDefault();
-        document.getElementById("timestamp-input-field").focus();
-    }
+  //Select timestamp input field
+  if (
+    (event.key === "w" || event.key === "W" || event.key === "田") &&
+    !formElements.includes(event.target.tagName)
+  ) {
+    event.preventDefault();
+    document.getElementById("timestamp-input-field").focus();
+  }
 
-    //Clear timestamp input field
-    if ((event.key === "e" || event.key === "E" || event.key === "水") && !formElements.includes(event.target.tagName)) {
-        event.preventDefault();
-        document.getElementById("timestamp-input-field").value = "";
-    }
+  //Clear timestamp input field
+  if (
+    (event.key === "e" || event.key === "E" || event.key === "水") &&
+    !formElements.includes(event.target.tagName)
+  ) {
+    event.preventDefault();
+    document.getElementById("timestamp-input-field").value = "";
+  }
 
-    //Deselect timestamp input field
-    if (event.key === "Escape" || event.key === "Escape") {
-        event.preventDefault();
-        document.getElementById("timestamp-input-field").blur();
-    }
+  //Deselect timestamp input field
+  if (event.key === "Escape" || event.key === "Escape") {
+    event.preventDefault();
+    document.getElementById("timestamp-input-field").blur();
+  }
 });
